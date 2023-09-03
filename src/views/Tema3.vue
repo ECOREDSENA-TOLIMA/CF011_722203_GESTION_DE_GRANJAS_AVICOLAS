b637c9719fb4847e12a253670bb85898825b444c
Oscar Espitia <odespitia@sena.edu.co>
2023-09-02 20:10:20 -0500

    temas 3.1, 3.2

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -50,6 +50,284 @@
     #t_3_2.titulo-segundo.color-acento-contenido(data-aos="fade-right")
       h2 3.2 Observación
 
+    .row.justify-content-center.align-items-center.mb-4(data-aos="fade-down")
+      .col-md-2
+        img(src='@/assets/curso/temas/tema3/tema3-6.svg', alt='')
+      .col-md-10
+        p Es la forma en la que el ser humano percibe su entorno, a través de esta acción se registran los hechos y fenómenos que suceden en un lugar y tiempo determinados. Funciona como un sistema de recolección de datos puesto que permite identificar, sobre todo, fenómenos sociales asociados a algún estímulo en particular.
+        p A diferencia de otras metodologías, la observación puede ser practicada por una sola persona, lo que puede facilitar su ejecución, en contextos donde se quiera evitar la distorsión de la información por la pluralidad de opiniones. 
+        p La observación puede aplicarse en diferentes etapas de la investigación: 
+
+    PasosA.color-acento-contenido.mb-5(tipo="n" data-aos="fade-down")
+      .row
+        .col-md-7.mb-4.mb-md-0(data-aos="down-left")
+          h4 Primera etapa
+          p.mb-0 Mediante su uso puede identificarse la problemática que requiere estudio para ser abordada. Por ejemplo, observar las gallinas de una granja avícola productora de huevos, podría indicar si algunas de ellas están presentando problemas de salud al evaluar su condición corporal.
+        .col-md-5
+          figure
+            img(src='@/assets/curso/temas/tema3/tema3-7.png', alt='')
+      .row
+        .col-md-5(data-aos="down-left")
+          figure
+            img(src='@/assets/curso/temas/tema3/tema3-8.png', alt='')
+        .col-md-7.mb-4.mb-md-0(style="text-align:left")
+          h4 Segunda etapa
+          p.mb-0 Otra etapa donde la observación toma valor es aquella aproximación que se efectúa una vez se es consciente de la problemática y se espera generar un diagnóstico inicial para hacerse una idea de su dimensión. Siguiendo con el ejemplo, se efectuaría un análisis más detallado a un número significativo de aves para evaluar si hay patologías en común o solo casos aislados que no ameritan atención.
+      .row
+        .col-md-7.mb-4.mb-md-0(data-aos="down-left")
+          h4 Tercera etapa
+          p.mb-0 Una última etapa consiste en la recolección efectiva de los datos, haciendo uso de los criterios de validez y fiabilidad necesarios para hacer las afirmaciones que apliquen. Para el caso de la granja avícola, en esta fase sería necesario registrar las condiciones de la muestra seleccionada con el apoyo de médicos veterinarios, y a partir de su opinión evaluar las medidas a tomar.
+        .col-md-5
+          figure
+            img(src='@/assets/curso/temas/tema3/tema3-9.png', alt='')
+
+    .titulo-segundo.color-acento-contenido(data-aos="fade-right")
+      h2 Recomendaciones para el observador
+
+    p.mb-5(data-aos="fade-down") Hacer uso de esta metodología requiere de aplicar una serie de acciones que permitan generar la mayor cantidad de información posible:
+
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-5.col-md-6.col-12(data-aos="fade-down")
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/tema3/tema3-10.svg' alt='')
+          .tarjeta.color-acento-botones
+            .p-4
+              p.mb-0 Es importante definir el objeto de la observación para así identificar qué se observa y para qué. Los aspectos a los que se les debe prestar mayor atención se determinan en función de lo que se espera conseguir con el trabajo de investigación.
+      .col-lg-5.col-md-6.col-12(data-aos="fade-down")
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/tema3/tema3-11.svg' alt='')
+          .tarjeta.color-acento-contenido
+            .p-4
+              p.texto-negro Definir y elaborar las herramientas adecuadas para que la observación siga un esquema en concreto y los datos obtenidos puedan registrarse de la manera más adecuada.
+      .col-lg-5.col-md-6.col-12(data-aos="fade-down")
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/tema3/tema3-12.svg' alt='')
+          .tarjeta.tarjeta--azul
+            .p-4
+              p Ejecutar una correcta planeación en la que se definan las actividades previas, aquellas que pondrán el objeto en cuestión, en las condiciones necesarias para llevar a cabo la labor de observación.
+      .col-lg-5.col-md-6.col-12(data-aos="fade-down")
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/tema3/tema3-13.svg' alt='')
+          .tarjeta.color-secundario
+            .p-4
+              p Definir el equipo de trabajo asignando las tareas y responsabilidades, esto solo aplica si la investigación requiere de más personas, ya sean expertos o colaboradores.
+
+    .titulo-segundo.color-acento-contenido(data-aos="fade-right")
+      h2 Modalidades de observación
+
+    p.mb-5(data-aos="fade-down") La observación puede ser clasificada de diferentes formas según criterios como los medios utilizados, la participación del observador, entre otros. La siguiente figura 4 relaciona los más relevantes:
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-12.col-md-10(data-aos="fade-right")
+        .titulo-sexto.color-primario
+          p.titulo.pb-0.mb-0 #[b Figura 4.] #[i Modalidades de observación.] 
+        figure
+          img(src='@/assets/curso/temas/tema3/tema3-14.svg', alt='En la figura 4. Se muestra un diagrama con diversas formas de observación utilizadas en la investigación y estudio de fenómenos.')  
+          figcaption.mt-2 Nota. Adaptada de Emanuelli et al. (2012).
+
+    .cajon-b.color-acento-botones.p-3.mb-4(data-aos="fade-down")
+      p.mb-4 A continuación, se ampliará la información sobre cada una de estas modalidades. 
+
+    .titulo-segundo.color-acento-contenido(data-aos="fade-right")
+      h2 Según medios utilizados
+    
+    p.mb-5(data-aos="fade-down") Bajo este sistema se evalúa el grado de estructuración utilizado por el observador, tanto para recolectar los datos como para registrarlos:
+
+    .row.justify-content-center.align-items-center.mb-4.con-borde
+      .col-12(data-aos="fade-down")
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/tema3/tema3-15.svg' alt='')
+          .tarjeta.color-acento-contenido--borde
+            .p-4
+              h4 Estructurada
+              p.mb-0 Cuenta con un trabajo de planeación detrás, en la que se utilizan procedimientos estructurados para la recopilación de la información. Usualmente limita el campo de observación pues desde un principio se define el alcance del estudio.
+      .col-12(data-aos="fade-down")
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/tema3/tema3-16.svg' alt='')
+          .tarjeta.color-primario--borde
+            .p-4
+              h4 No estructurada
+              p.mb-0 Se caracteriza por no seguir un formato de registro de los datos muy estructurado, se suele utilizar para el reconocimiento de los objetos a estudiar.
+
+    .titulo-segundo.color-acento-contenido(data-aos="fade-right")
+      h2 Según participación del observador
+    
+    p.mb-5(data-aos="fade-down") Se evalúa la participación del investigador en el campo objeto de estudio:
+
+    .row.justify-content-center.align-items-center.mb-4.con-borde
+      .col-12(data-aos="fade-down")
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/tema3/tema3-17.svg' alt='')
+          .tarjeta.color-acento-contenido--borde
+            .p-4
+              h4 Participante
+              p.mb-0 Si el observador se hace partícipe de los eventos observados ejecutando acciones que hacer parte del fenómeno a estudiar. Usualmente requiere preparación previa para adoptar las medidas que hagan la interacción más natural.
+      .col-12(data-aos="fade-down")
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/tema3/tema3-18.svg' alt='')
+          .tarjeta.color-primario--borde
+            .p-4
+              h4 No participante
+              p.mb-0 Existe contacto con el objeto de estudio, pero no participa en los hechos. Toma una postura como espectador en la que tiene plena consciencia de la situación.
+
+    .titulo-segundo.color-acento-contenido(data-aos="fade-right")
+      h2 Según aproximación al objeto
+    
+    p.mb-5(data-aos="fade-down") Bajo este criterio, el elemento diferenciador consiste en la cercanía que tiene el observador con la situación estudiada. Si este es el que efectúa las pruebas y analiza los eventos, o, por el contrario, parte del trabajo efectuado por terceros:
+
+    .row.justify-content-center.align-items-center.mb-4.con-borde
+      .col-12(data-aos="fade-down")
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/tema3/tema3-19.svg' alt='')
+          .tarjeta.color-acento-contenido--borde
+            .p-4
+              h4 Directa
+              p.mb-0 El observador es testigo de la situación de primera mano, evalúa el fenómeno y alcanza conclusiones a partir de ello. La calidad de la información dependerá de la pericia del investigador, y de lo estructurado que se encuentre el proceso.
+      .col-12(data-aos="fade-down")
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/tema3/tema3-20.svg' alt='')
+          .tarjeta.color-primario--borde
+            .p-4
+              h4 Indirecta
+              p.mb-0 La recolección de los datos no proviene de los hechos propiamente dichos, sino de las fuentes secundarias de información. En este sentido, el investigador se vale del trabajo previo de otros investigadores como encuestas, cuestionarios o trabajos académicos.
+    
+    .titulo-segundo.color-acento-contenido(data-aos="fade-right")
+      h2 Según el lugar
+    
+    p.mb-5(data-aos="fade-down") Evalúa las condiciones en las que se presenta el fenómeno observable:
+
+    .row.justify-content-center.align-items-center.mb-4.con-borde
+      .col-12(data-aos="fade-down")
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/tema3/tema3-21.svg' alt='')
+          .tarjeta.color-acento-contenido--borde
+            .p-4
+              h4 En la vida real
+              p.mb-0 Sucede sin ningún tipo de participación del observador, tal cual suceden los hechos en una situación real sin ninguna preparación extraordinaria.
+      .col-12(data-aos="fade-down")
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/tema3/tema3-22.svg' alt='')
+          .tarjeta.color-primario--borde
+            .p-4
+              h4 Experimental
+              p.mb-0 En este caso los eventos son manipulados por el investigador. Son situaciones provocadas para aproximarlas a lo que se busca estudiar, pero sin afectar su estructura.
+
+
+    .titulo-segundo.color-acento-contenido(data-aos="fade-right")
+      h2 Instrumentos para la recolección de información
+
+    p.mb-5(data-aos="fade-down") Para llevar a cabo la observación de una forma estructurada, es importante hacer uso de herramientas que permitan recopilar la información de una manera organizada y controlada. Con esto se pulirá aún más la labor de investigación, facilitando el análisis y la interpretación. Los siguientes son algunos de los medios más utilizados:
+
+    TabsA.color-acento-botones.mb-5
+      .tarjeta.tarjeta--azul.p-4(titulo="El diario")
+        .row
+          .col-md-8.col-12
+            h4 El diario
+            p En este se inscriben paso a paso todos los hechos observados desde el inicio del proyecto. Pueden anotarse todo tipo de comentarios, sugerencias o notas de aspectos teóricos o prácticos. Con este se busca llevar un registro tan detallado como se quiera de la evolución del hecho estudiado, así que se puede diligenciar después de una tarea, de una situación relevante o al final de la jornada de trabajo.
+            p Se recomienda una escritura clara, precisa y ordenada de acuerdo con lo observado, su extensión variará según la complejidad del objeto de estudio o de la preferencia del investigador.
+          .col-md-4.col-12
+            img(src='@/assets/curso/temas/tema3/tema3-23.png', alt='')
+      .tarjeta.tarjeta--azul.p-4(titulo="Cuaderno de notas")
+        h4 Cuaderno de notas
+        .row
+          .col-md-8.col-12
+            p Funciona de manera similar al diario con la particularidad de que el investigador lo tendrá a mano en todo momento. Debido a esto es probable que no tenga una estructura tan ordenada, pero aportará información mucho más detallada dado que el investigador hará las anotaciones en el momento que identifique una situación que así lo amerite.
+            p Puede ser un complemento para el diario, de tal forma que se registren los hechos de manera ágil y menos ordenada, para luego trasladarse de forma más estructurada al diario.
+          .col-md-4.col-12
+            img(src='@/assets/curso/temas/tema3/tema3-24.png', alt='')
+      .tarjeta.tarjeta--azul.p-4(titulo="Mapas")
+        h4 Mapas
+        .row
+          .col-md-8.col-12
+            p Son una herramienta que aporta gran utilidad para ofrecer un mejor contexto a la investigación. En esta categoría también se incluyen los planos y cualquier gráfico que otorgue una noción respecto a la ubicación o límites de la zona objeto estudio.
+            p Se recomienda detallarlos con toda la información que pueda resultar relevante para el estudio, por ejemplo, si se trata de un estudio de mercado, se puede delimitar la localidad a partir de un mapa del municipio y agregar información como la edad promedio, estrato socioeconómico o ingreso per cápita, entre otros.
+          .col-md-4.col-12
+            img(src='@/assets/curso/temas/tema3/tema3-25.png', alt='')
+      .tarjeta.tarjeta--azul.p-4(titulo="Dispositivos mecánicos")
+        h4 Dispositivos mecánicos
+        .row
+          .col-md-8.col-12
+            p Son las herramientas electrónicas como las cámaras o grabadoras que otorgan información objetiva y exacta. Es importante resaltar que requieren de mucha precaución porque según el tipo de estudio, puede verse comprometida la privacidad de las personas si no se cuenta con su autorización correspondiente.
+          .col-md-4.col-12
+            img(src='@/assets/curso/temas/tema3/tema3-26.png', alt='')
+      .tarjeta.tarjeta--azul.p-4(titulo="Cuadros de trabajo")
+        h4 Cuadros de trabajo
+        .row
+          .col-md-8.col-12
+            p Consiste en la tabulación de los datos obtenidos por las otras herramientas. Pueden diseñarse tablas en las que las filas representen los participantes y las columnas las respuestas dadas por cada uno de ellos.
+            p Las posibilidades son tan variadas como la imaginación del investigador, por lo que es una herramienta muy aconsejable para casos que involucran muchos sujetos a observar, o cuando los datos obtenidos son de naturaleza cuantitativa, puesto que posibilitan las operaciones aritméticas.
+          .col-md-4.col-12
+            img(src='@/assets/curso/temas/tema3/tema3-27.png', alt='')
+
+    .cajon-b.color-acento-contenido.p-3.mb-5
+      p A continuación, se presenta un ejemplo en el que un concesionario quiere identificar los colores más demandados para cada uno de sus modelos de vehículos, por lo que se anotaron el modelo y el color de todos los carros que ingresaron a revisión periódica en una semana, ver tabla 2:
+
+    .row.justify-content-center.align-items-center.mb-4
+      .col-12.col-md-11.col-lg-10(data-aos="fade-right")
+        .titulo-sexto.color-primario
+          p.titulo.pb-0.mb-0 #[b Tabla 2.] #[i Formato cuadro de trabajo.] 
+      .col-12.col-md-11.col-lg-10(data-aos="fade-right")
+        .tabla-b.color-acento-contenido.mb-5
+          table
+            thead
+              tr.cabecera-a.text-center
+                th Color
+                th Modelo 1
+                th Modelo 2
+                th Modelo 3
+                th Total
+            tbody
+              tr.text-center
+                th.cabecera-b Blanco
+                td 8
+                td 9
+                td 9
+                td 26
+              tr.text-center
+                th.cabecera-b Negro
+                td 10
+                td 7
+                td 8
+                td 25
+              tr.text-center
+                th.cabecera-b Rojo
+                td 6
+                td 8
+                td 9
+                td 23
+              tr.text-center
+                th.cabecera-b Gris
+                td 5
+                td 6
+                td 7
+                td 18
+              tr.text-center
+                th.cabecera-b Total
+                td 29
+                td 30
+                td 33
+                td 92
+
+    .tarjeta.color-primario.p-4.mb-5(data-aos="fade-down-left")
+      .row.justify-content-around.align-items-center
+        .col-md-2.col-lg-2.mb-4.mb-md-0
+          img(src="@/assets/curso/temas/tema1/tema1-5.png")
+        .col-md-10.col-lg-10
+          p.mb-0 La elaboración del cuadro anterior permite analizar la información desde una nueva perspectiva y alcanzar nuevas conclusiones. Por ejemplo, que el modelo 3 tiene mayor aceptación a nivel comercial, que el color más común es el blanco o que la combinación más popular es el modelo 1 de color negro, entre otros. 
+
+
 
 </template>
 
